c16eaab4abef295360097250994bd51544f32164
achraf-bit <achraf.bouchouik@gmail.com>
2021-03-11 08:49:06 +0100

    first one

---
+++ src/main/java/org/sid/demoweb/DemoWebApplication.java
@@ -0,0 +1,29 @@
+package org.sid.demoweb;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebApplication.class, args);
+	}
+	@GetMapping("/test")
+	public  Map<String,Object> test(){
+		Map<String,Object> data=new HashMap<>();
+		data.put("name","from docker");
+		data.put("counter",456);
+		return data;
+
+
+	}
+
+
+}
